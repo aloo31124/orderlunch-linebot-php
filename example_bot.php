1d12978b94959261f8f6674de80fad516be93b2d
aloo31124 <aloo31124@gmail.com>
2021-12-21 22:57:55 +0800

    取得 token 函示初步測試2

--- example_bot.php
+++ example_bot.php
@@ -61,6 +61,7 @@ foreach ($client->parseEvents() as $event) {
                 case 'text': //訊息為文字
                     require_once('includes/text.php'); //Type: Text
                     require_once('includes/getPlacesApi.php'); //Type: Text
+                    require_once('includes/postTokenApi.php'); //Type: Text
                     require_once('includes/image.php'); //Type: Image
                     require_once('includes/video.php'); //Type: Video
                     require_once('includes/audio.php'); //Type: Audio
