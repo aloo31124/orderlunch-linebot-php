f75136a7472b53d4727fab0a4714f020631c9a01
aloo31124 <aloo31124@gmail.com>
2021-12-21 22:18:06 +0800

    回傳 file_get_contents

--- includes/getplacesapi.php
+++ includes/getplacesapi.php
@@ -6,8 +6,8 @@ if ($message['text'] == "get api") {
         'replyToken' => $event['replyToken'],
         'messages' => array(
             array(
-                'type' => 'text', //訊息類型 (文字)
-                'text' => "取得api: " //回覆訊息
+                'type' => 'text', 
+                'text' => "取得api: " + file_get_contents("https://googleplacesapi.herokuapp.com/places") 
             )
         )
     ));
