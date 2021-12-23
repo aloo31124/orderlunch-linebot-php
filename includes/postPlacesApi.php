d00293bbb0de0c52b11e6a9bcf9c721abe6267a1
aloo31124 <aloo31124@gmail.com>
2021-12-23 19:28:13 +0800

    feat : postPlacesApi 5

--- includes/postPlacesApi.php
+++ includes/postPlacesApi.php
@@ -16,8 +16,8 @@ if ($message['text'] == "post places") {
 
 function postPlaces(){
     $postData = array(
-        'lat' => '0487.2',
-        'lng' => '0487.23'
+        'lat' => '99999.2',
+        'lng' => '11111.23'
     );
 
     $ch = curl_init("https://googleplacesapi.herokuapp.com/places");
@@ -26,39 +26,7 @@ function postPlaces(){
         CURLOPT_RETURNTRANSFER => TRUE,
         CURLOPT_HTTPHEADER => array(
             'Content-Type: application/json',
-            'Authorization: basic '.getToken()
-        ),
-        CURLOPT_POSTFIELDS => json_encode($postData)
-    ));
-
-    $response = curl_exec($ch);
-
-    if($response === FALSE){
-        die(curl_error($ch));
-    }
-
-    // Decode the response
-    $responseData = json_decode($response, TRUE);
-
-    // Close the cURL handler
-    curl_close($ch);
-
-    // Print the date from the response
-    return $responseData['token'];
-}
-
-function getToken(){
-    $postData = array(
-        'username' => 'adm',
-        'password' => 'QazWsxEdc~'
-    );
-
-    $ch = curl_init("https://googleplacesapi.herokuapp.com/auth");
-    curl_setopt_array($ch, array(
-        CURLOPT_POST => TRUE,
-        CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
+            'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbSIsImV4cCI6MTY0MDI2MjM0MX0.azh5kUod-_0C8bSWI4YuZE04kRaPmnPZ42mi7lrzhuM'
         ),
         CURLOPT_POSTFIELDS => json_encode($postData)
     ));
